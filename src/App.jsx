74dd0391bbd2ca2a6df9c3eb3e2cc55ef518a880
aabhushanCD <dammardhakal426@gmail.com>
2024-04-11 20:33:06 +0545

    header ,footer slideBar using boots strap completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Components/header";
+import Footer from "./Components/Footer";
+import Sidebar from "./Components/sidebar";
+function App() {
+  return (
+    <>
+      <div className="slider_header">
+        <Sidebar></Sidebar>
+        <Header></Header>
+      </div>
+      <div>thiis is sfjkasdfjaskdfjklsjdklfjksbodey</div>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
